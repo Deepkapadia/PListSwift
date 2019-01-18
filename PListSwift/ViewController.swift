ac4d1ce6333cdf786f6dcf54820cb59c3a31bc17
Deep_Kapadia <kapadia.deep12@gmail.com>
2019-01-18 11:20:46 +0530

    PList_Swift
    PList File Create nad Display it - Swift Sample.

--- PListSwift/ViewController.swift
+++ PListSwift/ViewController.swift
@@ -20,6 +20,43 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btncreatePlist(_ sender: Any)
+    {
+        let docDir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String;
+        let path=docDir.appending("/profile.plist");
+        
+        if(!FileManager().fileExists(atPath: path))
+        {
+            print(path);
+            
+            let data : [String: String] = [
+                "Company": "My Company",
+                "FullName": "My Full Name",
+                "FirstName": "My First Name",
+                "LastName": "My Last Name",
+                // any other key values
+            ];
+            
+            let someData = NSDictionary(dictionary: data);
+            let isWritten = someData.write(toFile: path, atomically: true);
+            print("is the file created: \(isWritten)");
+            
+        }
+        else
+        {
+            print("file exists");
+        }
+    }
+    
+    @IBAction func btnDisplayPlist(_ sender: Any)
+    {
+        let document = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString;
+        let path = document.appending("/profile.plist");
+        
+        let somedata = NSDictionary(contentsOfFile: path);
+        
+        print(somedata!);
+    }
 
 }
 
